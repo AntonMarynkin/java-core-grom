b307bc29ad7f05bcac887db08421491123c4acd7
Toha <anmarynkin@gmail.com>
2021-05-21 19:19:37 +0300

    Class Deal add

---
+++ src/lesson6/Deal.java
@@ -0,0 +1,58 @@
+package lesson6;
+
+import java.util.Date;
+
+public class Deal {
+    public static void main(String[] args) {
+        confirmDeal();
+        System.out.println(checkPrice());
+        System.out.println(isValidType());
+    }
+
+    long id;
+    static int price;
+    Date dateCreated;
+    static boolean isConfirmed;
+    static Date dateConfirmed;
+    String city;
+    String country;
+    static String type;
+
+    public Deal(int price, Date dateCreated, boolean isConfirmed, Date dateConfirmed, String city, String country, String type) {
+        this.price = price;
+        this.dateCreated = dateCreated;
+        this.isConfirmed = isConfirmed;
+        this.dateConfirmed = dateConfirmed;
+        this.city = city;
+        this.country = country;
+        this.type = type;
+    }
+
+    public Deal() {
+    }
+
+
+    public static void confirmDeal() {
+        isConfirmed = true;
+        dateConfirmed = new Date();
+        System.out.println(new Date());
+        System.out.println(isConfirmed);
+    }
+
+    public static boolean checkPrice() {
+        if (price > 1000)
+            isConfirmed = true;
+        else
+            isConfirmed = false;
+
+        return isConfirmed;
+    }
+
+    public static boolean isValidType() {
+        if (type == "Buy" || type == "Sale")
+            isConfirmed = true;
+        else
+            isConfirmed = false;
+        return isConfirmed;
+    }
+}
