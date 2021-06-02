f0d72165d1e57035e9fc279000ebf4f5aea9afad
Toha <anmarynkin@gmail.com>
2021-06-02 15:12:51 +0300

    test lesson9 add

---
+++ src/lesson3/testLesson9/FinancialCompany.java
@@ -0,0 +1,10 @@
+package lesson3.testLesson9;
+
+import lesson9.Company;
+
+public class FinancialCompany extends Company {
+
+    void demonstrateProtectedModifier() {
+        System.out.println(this.code);
+    }
+}
