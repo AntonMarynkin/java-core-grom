2f076619d882dd2cced3cc7137287df41014e700
Toha <anmarynkin@gmail.com>
2021-06-19 15:34:32 +0300

    update classes

--- src/lesson9/homework/Demo.java
+++ src/lesson9/homework/Demo.java
@@ -5,13 +5,17 @@ public class Demo {
         User client = new User(65121321, "Alex", "session2");
         User client2 = new User(65171321, "Sergiy", "session3");
         User client3 = new User(65421321, "Ivan", "session4");
-        User[] clients = {client, client2, client3};
+        User[] clients = {client, client2, client3, null, null};
 
         UserRepository userRepository = new UserRepository(clients);
 
         userRepository.getUserByName("Vadim");
         userRepository.getUserById(377378734);
         userRepository.getUserBySessionId("session1");
+
         userRepository.save(client);
+
+        userRepository.update(client);
+        userRepository.delete(377378734);
     }
 }
--- src/lesson9/homework/UserRepository.java
+++ src/lesson9/homework/UserRepository.java
@@ -28,6 +28,8 @@ public class UserRepository {
 
     public User getUserByName(String name) {
         for (User findUser : users) {
+            if (findUser == null)
+                continue;
             if (name == findUser.getName())
                 return findUser;
         }
@@ -36,6 +38,8 @@ public class UserRepository {
 
     public User getUserById(long id) {
         for (User findUser : users) {
+            if (findUser == null)
+                continue;
             if (id == findUser.getId())
                 return findUser;
         }
@@ -44,6 +48,8 @@ public class UserRepository {
 
     public User getUserBySessionId(String sessionId) {
         for (User findUser : users) {
+            if (findUser == null)
+                continue;
             if (sessionId == findUser.getSessionId())
                 return findUser;
         }
@@ -55,16 +61,43 @@ public class UserRepository {
         for (User findUser : users) {
             if (findUser.getId() != user.getId() && user != findById(523852387))
                 user = users[users.length - 1];
-                return user;
+            return user;
         }
         return null;
     }
 
-    private User findById(long id) {
+    public User findById(long id) {
         for (User findUser : users) {
+            if (findUser == null)
+                continue;
             if (id == findUser.getId())
                 return findUser;
         }
         return null;
+    }
+
+
+    public User update(User user) {
+        for (User findUser : users) {
+            if (findUser.getId() == user.getId() && user == findById(523852387))
+                user = users[users.length - 1];
+            return user;
+        }
+        return null;
+    }
+
+    public void delete(long id) {
+        long delId = 0;
+        String delName = null;
+        String delSessionId = null;
+        for (User findUser : users) {
+            if (findUser == null)
+                continue;
+            if (id == findUser.getId()) {
+                getUserByName(delName);
+                getUserById(delId);
+                getUserBySessionId(delSessionId);
+            }
+        }
     }
 }
