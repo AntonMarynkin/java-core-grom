a47fda0f111a4a67fc252e9e17487a9f2edb68bc
Toha <anmarynkin@gmail.com>
2021-11-28 15:24:40 +0200

    lesson 15 homework

---
+++ src/lesson15/hw/Demo.java
@@ -0,0 +1,16 @@
+package lesson15.hw;
+
+public class Demo {
+    public static void main(String[] args) {
+        User user1 = new User(65465465, "Name", "4f65we4f6w5e");
+        User user2 = new User(65461111, "Name2", "4f65we4f6w5e");
+        User user3 = new User(65465465, "Name", "4f65we4f6w5e");
+        User[] users = new User[10];
+        UserRepository userRepository = new UserRepository(users);
+
+        System.out.println(user1.equals(user3));
+        System.out.println(userRepository.save(user1));
+
+        System.out.println(userRepository.update(user2));
+    }
+}
